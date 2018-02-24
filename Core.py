8b8da4bd915db23d4e821498fb30b4911703ee3d
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2018-02-24 15:43:19 +0100

    add params URl + validator + URL response 200

--- Core.py
+++ Core.py
@@ -1,6 +1,7 @@
 import ssl
 import time
 
+from urllib.parse import urlparse
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -9,21 +10,54 @@ class CoreTest :
 
 
     url_page = ""
+    domain = ""
 
     page_source_code = ""
     soup = ""
 
     def __init__(self, url_page):
         self.url_page = url_page
+        self.url_parsed = urlparse(url_page)
+
+        #global infos from URL
+        self.domain = self.url_parsed.netloc
+        self.protocol = self.url_parsed.scheme
+        self.path = self.url_parsed.path
+        self.query = self.url_parsed.query
+        self.params = self.url_parsed.params
+        self.fragment = self.url_parsed.fragment
+
+
         self.page_source_code = self.sourceCode()
         self.soup = self.soupify()
 
 
 
+    # URL management
     def getUrlPage(self):
         return self.url_page
     def setUrlPage(self, url):
         self.url_page = url
+
+    def getUrlParsed(self):
+        return self.url_parsed # return scheme = http(s) netloc (domain) path (template) params query fragment
+
+    # global infos form URL
+    def getDomain(self):
+        return self.domain
+    def getProtocol(self):
+        return self.url_parsed.scheme
+    def getPath(self):
+        return self.url_parsed.path
+    def getQuery(self):
+        return self.url_parsed.query
+    def getParams(self):
+        return self.url_parsed.params
+    def getFragment(self):
+        return self.url_parsed.fragment
+
+
+
 
 
     #Page Source Code management
@@ -52,7 +86,6 @@ class CoreTest :
         return soup
 
 
-
     #Results tests
     def renderTests(self):
         t0 = time.time()
@@ -66,9 +99,20 @@ class CoreTest :
         print(" _____________________________ RESULTS TESTS _____________________________ ")
         print("\n")
         print("\n")
-        print(" URL target : " + self.url_page)
+        print(" URL : " + self.url_page)
+        print(" Protocol : " + self.protocol)
+        print(" Domain : " + self.domain)
+        print(" Path : " + self.path)
+        print(" Quey : " + self.query)
+        print(" Params : " + self.params)
+        print(" Fragment : " + self.fragment)
+        print("")
+        print("\n")
         print(" Scripts tag : " + str(scriptTags))
         print("\n")
         print("\n")
         t1 = time.time()
         return "Execution time for each tests : " + str(t1-t0)
+
+
+
--- weblint.py
+++ weblint.py
@@ -1,10 +1,45 @@
 
+import sys
+import requests
+
+from django.core.validators import URLValidator
 from Core import CoreTest
-coreTest = CoreTest('https://www.sslshopper.com/ssl-checker.html')
 
-print(coreTest.getUrlPage())
+isValid = False
 
-print(coreTest.getPageSourceCode())
-print(coreTest.getSoupPretty())
+if(sys.argv.__len__() < 2):
+    print("Please enter an url to start the test")
+else:
+    url = str(sys.argv[1])
+    val = URLValidator()
+    try:
+        val(url)
+        isValid = True
+        if (isValid):
+            print(" URL is valid")
+            req = requests.get(url)
+            stauts_code = req.status_code
+            if (stauts_code == 200):
+                coreTest = CoreTest(str(sys.argv[1]))
 
-print(coreTest.renderTests())
+                print(coreTest.getUrlPage())
+
+                print(coreTest.getPageSourceCode())
+                print(coreTest.getSoupPretty())
+
+                print(coreTest.renderTests())
+
+                print(coreTest.getUrlParsed()) #For test
+
+            else:
+                print("Error : " + str(stauts_code))
+        else:
+            print(" URL given not valid !")
+    except:
+        isValid = False
+
+
+
+
+
+
